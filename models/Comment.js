4baf744d90d2b46803840e5491d4981b22d5abcf
Jennifer Winter <jennwintr@gmail.com>
2024-01-25 12:39:05 -0500

    models

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,42 @@
 const { Sequelize, Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/config');
+const sequelize = require("../config/connection");
 
 class Comment extends Model {}
-
 Comment.init(
   {
-    body: {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment_text: {
       type: DataTypes.STRING,
-      allowNull: false
-    }
+      validate: {
+        len: [3],
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
   },
   {
-    sequelize
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
   }
 );
-
 module.exports = Comment;
--- models/index.js
+++ models/index.js
@@ -2,19 +2,32 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-Post.belongsTo(User, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE'
+User.hasMany(Post, {
+  foreignKey: "user_id",
 });
-
-Post.hasMany(Comment, {
-  foreignKey: 'postId',
-  onDelete: 'CASCADE'
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
 });
 
 Comment.belongsTo(User, {
-  foreignKey: 'userId',
-  onDelete: 'CASCADE'
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+  onDelete: "cascade",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+  onDelete: "cascade",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+  onDelete: "cascade",
 });
 
 module.exports = {
